0104918ac27426df8fafb34e24d5c0d923ea3dea
Christopher Lucinski <clucinski@gmail.com>
2024-04-06 02:27:52 -0400

    Add refreshing token and getting saved songs

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
 import os
+import time
 
 load_dotenv()
 
@@ -10,20 +11,71 @@ app = Flask(__name__)
 
 app.secret_key = "fehw9r3829hr3298eFSEWw3G3FpJC3D8"
 app.config['SESSION_COOKIE_NAME'] = 'Chris Cookie'
+TOKEN_INFO = "token_info"
 
 @app.route('/')
 def login():
     sp_oauth = create_spotify_oauth()
     auth_url = sp_oauth.get_authorize_url()
+    print(auth_url)
     return redirect(auth_url)
 
 @app.route('/redirect')
 def redirectPage():
-    return 'redirect'
+    sp_oauth = create_spotify_oauth()
+    session.clear()
+    # save session token
+    code = request.args.get('code')
+    token_info = sp_oauth.get_access_token(code)
+    session[TOKEN_INFO] = token_info
+    return redirect(url_for('getTracks', _external=True))
 
 @app.route('/getTracks')
 def getTracks():
-    return 'Some drake songs or something'
+    session['token_info'], authorized = get_token()
+    session.modified = True
+    if not authorized:
+        return redirect('/')
+    
+    sp = spotipy.Spotify(auth=session.get('token_info').get('access_token'))
+
+    # Gather saved songs
+    results = []
+    iter = 0
+    while True:
+        offset = iter * 50
+        iter += 1
+        curGroup = sp.current_user_saved_tracks(limit=50, offset=offset)['items']
+        for idx, item in enumerate(curGroup):
+            track = item['track']
+            val = track['name'] + " - " + track['artists'][0]['name']
+            results += [val]
+        if (len(curGroup) < 50):
+            break
+    
+    return str(len(results))  #for now, return number of saved songs
+
+# Check that token is valid, and get a new one if it isn't
+def get_token():
+    token_valid = False
+    token_info = session.get("token_info", {})
+
+    # Checking if the session already has a token stored
+    if not (session.get('token_info', False)):
+        token_valid = False
+        return token_info, token_valid
+
+    # Checking if token has expired
+    now = int(time.time())
+    is_token_expired = session.get('token_info').get('expires_at') - now < 60
+
+    # Refreshing token if it has expired
+    if (is_token_expired):
+        sp_oauth = create_spotify_oauth()
+        token_info = sp_oauth.refresh_access_token(session.get('token_info').get('refresh_token'))
+
+    token_valid = True
+    return token_info, token_valid
 
 
 def create_spotify_oauth():
